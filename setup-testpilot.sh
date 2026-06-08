4c870ad671cc76da17373d9dd28e91a31aa1938b
loiclefevre <loic.lefevre@gmail.com>
2026-06-08 15:48:49 +0200

    4+5 - manage proper download retries and retry_count for create operation

--- setup-testpilot.sh
+++ setup-testpilot.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -f "${GITHUB_ACTION_PATH}"/setup-testpilot ]; then
 
 echo "::group::🔽 Downloading setup-testpilot"
-wget https://github.com/oracle-actions/setup-testpilot/releases/download/${VERSION}/setup-testpilot-linux-x86_64.tar.gz -O ${GITHUB_ACTION_PATH}/setup-testpilot-linux-x86_64.tar.gz -q
+wget --continue --progress=dot:mega --tries=0 https://github.com/oracle-actions/setup-testpilot/releases/download/${VERSION}/setup-testpilot-linux-x86_64.tar.gz -O ${GITHUB_ACTION_PATH}/setup-testpilot-linux-x86_64.tar.gz -q
 echo "::endgroup::"
 
 echo "::group::📦 Unpacking setup-testpilot"
